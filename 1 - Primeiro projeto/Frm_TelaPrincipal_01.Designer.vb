019748c8092c69b15b955549242c3c382af157eb
Leonardo Matheus Abreu <87587116+leonardomatheuss@users.noreply.github.com>
2022-08-31 08:37:53 -0300

    Add files via upload

---
+++ 1 - Primeiro projeto/Frm_TelaPrincipal_01.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frm_TelaPrincipal_01
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Btm_Principal = New System.Windows.Forms.Button()
+        Me.Lbl_NomeProjeto = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Btm_Principal
+        '
+        Me.Btm_Principal.Location = New System.Drawing.Point(113, 192)
+        Me.Btm_Principal.Name = "Btm_Principal"
+        Me.Btm_Principal.Size = New System.Drawing.Size(249, 62)
+        Me.Btm_Principal.TabIndex = 0
+        Me.Btm_Principal.Text = "Clique aqui!"
+        Me.Btm_Principal.UseVisualStyleBackColor = True
+        '
+        'Lbl_NomeProjeto
+        '
+        Me.Lbl_NomeProjeto.AutoSize = True
+        Me.Lbl_NomeProjeto.Location = New System.Drawing.Point(12, 9)
+        Me.Lbl_NomeProjeto.Name = "Lbl_NomeProjeto"
+        Me.Lbl_NomeProjeto.Size = New System.Drawing.Size(98, 13)
+        Me.Lbl_NomeProjeto.TabIndex = 1
+        Me.Lbl_NomeProjeto.Text = "1 - Primeiro Projeto "
+        '
+        'Frm_TelaPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 336)
+        Me.Controls.Add(Me.Lbl_NomeProjeto)
+        Me.Controls.Add(Me.Btm_Principal)
+        Me.Name = "Frm_TelaPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Alô mundo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btm_Principal As Button
+    Friend WithEvents Lbl_NomeProjeto As Label
+End Class
---
+++ 1 - Primeiro projeto/Frm_TelaPrincipal_01.vb
@@ -0,0 +1,17 @@
+﻿Public Class Frm_TelaPrincipal_01
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+
+    Private Sub Btm_Principal_Click(sender As Object, e As EventArgs) Handles Btm_Principal.Click
+
+        MsgBox("Olá mundo !!!", MsgBoxStyle.Critical, "Mensagem")
+
+
+    End Sub
+End Class
